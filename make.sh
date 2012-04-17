2e5c8d47d148d31f1938edbcbdef437a77a166d7
Stanislas Polu <stan@teleportd.com>
2012-04-17 09:51:52 +0200

    0.2

--- make.sh
+++ make.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-cat base.js > all.js
-cat event.js >> all.js
-cat cell.js >> all.js
+rm cell-0.*.min.js
 
-jsmin < all.js > cell-0.1.min.js
+cat src/base.js > all.js
+cat src/event.js >> all.js
+cat src/cell.js >> all.js
+
+jsmin < all.js > cell-0.2.min.js
 
 rm all.js
